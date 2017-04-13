bd036374bc5ecfaeb6943ba7000e9fe20388251e
zhongxh <zhxhyz@gmail.com>
2017-04-13 16:09:04 +0800

    implement edit & detroy post

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -3,4 +3,30 @@ class Account::PostsController < ApplicationController
   def index
     @posts = current_user.posts
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: 'Post update success'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path, alert: 'Post deledted'
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
   belongs_to :user
-  has_many :posts
+  has_many :posts, dependent: :destroy
   validates :title, presence: true
   has_many :group_relationships
   has_many :members, through: :group_relationships, source: :user
